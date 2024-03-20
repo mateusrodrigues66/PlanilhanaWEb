e70baaa0aa29d9b92ae05fddfbc026d29eb4c5bf
mateusrodrigues66 <93056159+mateusrodrigues66@users.noreply.github.com>
2024-03-20 07:48:08 -0300

    Update upload.php

--- upload.php
+++ upload.php
@@ -6,11 +6,6 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_FILES['image']) && $_FILES['
     $password = "";
     $database = "planilhaweb";
     $conn = new mysqli($host, $user, $password, $database, 666);
-    echo "entrou php";
-    if ($conn->connect_error) {
-        die("Falha na conexão: " . $conn->connect_error);
-    }
-
     $fileName = $_FILES['image']['name'];
     $fileType = $_FILES['image']['type'];
     $fileData = file_get_contents($_FILES['image']['tmp_name']);
@@ -23,17 +18,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_FILES['image']) && $_FILES['
     $stmt->bind_param("ssbi", $fileName, $fileType, $null, $fileSize);
     $stmt->send_long_data(2, $fileData); // dados binários
     $stmt->execute();
-
-    if ($stmt->affected_rows > 0) {
-        header('Location: nome_do_seu_arquivo_html.php?uploadSuccess');
-        echo "Arquivo enviado e salvo no banco de dados com sucesso.";
-    } else {
-        echo "Erro ao salvar o arquivo no banco de dados.";
-    }
     
     $stmt->close();
     $conn->close();
-} else {
-    echo "Nenhum arquivo enviado ou erro no upload.";
-}
 ?>
